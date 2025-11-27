c01f4cba0cb98ed4c78607c51e75027622844080
DemakovAndrii <demakov1155@gmail.com>
2025-11-27 21:22:14 +0000

    add db logs

--- config/database.ts
+++ config/database.ts
@@ -1,5 +1,10 @@
 export default ({ env }) => {
 	const client = env("DATABASE_CLIENT", "sqlite");
+
+	console.log("DATABASE_CLIENT_HERE:", client);
+	console.log("DATABASE_URL_HERE:", env("DATABASE_URL"));
+	console.log("USING_SSL_HERE:", env.bool("DATABASE_SSL", false));
+
 	return {
 		connection: {
 			client,
