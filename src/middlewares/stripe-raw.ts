8977576b48e81ad63893c1f24630bef55627b5cf
DemakovAndrii <demakov1155@gmail.com>
2025-11-28 13:13:19 +0000

    fix stripe raw

--- src/middlewares/stripe-raw.ts
+++ src/middlewares/stripe-raw.ts
@@ -1,15 +1,14 @@
 module.exports = (config, { strapi }) => {
 	return async (ctx, next) => {
 		if (ctx.request.url.startsWith("/api/stripe/webhook")) {
-			const raw = await new Promise((resolve) => {
-				let data = "";
-				ctx.req.on("data", (chunk) => {
-					data += chunk;
-				});
-				ctx.req.on("end", () => resolve(data));
+			const chunks: Buffer[] = [];
+
+			await new Promise<void>((resolve) => {
+				ctx.req.on("data", (chunk) => chunks.push(chunk));
+				ctx.req.on("end", () => resolve());
 			});
 
-			ctx.request.body = raw;
+			ctx.request.body = Buffer.concat(chunks);
 		}
 
 		await next();
